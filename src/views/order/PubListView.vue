6608634ccb5cadaafb9f552f1ff4b38043ae11db
wanghanlin <wanghanlin917@163.com>
2024-11-22 16:37:35 +0800

    2024.11.22 home

--- src/views/order/PubListView.vue
+++ src/views/order/PubListView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts"></script>
 <template>
-  <div>运单管理</div>
+  <el-card class="box-card" shadow="never" style="min-width: 1120px">
+    <template #header>
+      <div class="card-header">
+        <span style="font-weight: bold; font-size: 18px">运单管理</span>
+      </div>
+    </template>
+  </el-card>
 </template>
 
 <style scoped></style>
--- src/views/order/PubView.vue
+++ src/views/order/PubView.vue
@@ -1,6 +1,243 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+const fromDate = ref<any>({
+  from_addr: "",
+  from_name: "",
+  from_mobile: "",
+  from_date: ""
+})
+const fromDateError = ref<any>({
+  from_addr: "",
+  from_name: "",
+  from_mobile: "",
+  from_date: ""
+})
+const shortcuts = [
+  {
+    text: "今天",
+    value: new Date()
+  },
+  {
+    text: "明天",
+    value: () => {
+      const date = new Date()
+      date.setTime(date.getTime() + 3600 * 1000 * 24)
+      return date
+    }
+  }
+]
+const toDate = ref<any>({
+  to_addr: "",
+  to_mobile: "",
+  to_date: ""
+})
+const toDateError = ref<any>({
+  to_to_addr: "",
+  to_mobile: "",
+  to_date: ""
+})
+const goodInfo = ref<any>({
+  goods_type: "",
+  weight: "",
+  title: "",
+  unit_price: "",
+  cost: "",
+  remark: "",
+  goods_type_options: [
+    { value: 1, label: "电子产品" },
+    { value: 2, label: "大宗货物" },
+    { value: 3, label: "冷藏货物" },
+    { value: 4, label: "农产品" },
+    { value: 5, label: "其他" }
+  ]
+})
+const goodInfoError = ref<any>({
+  goods_type: "",
+  weight: "",
+  title: "",
+  unit_price: "",
+  cost: "",
+  remark: ""
+})
+const showDrawer = ref<boolean>(false)
+const tableData = ref<any>([
+  {
+    name: "2016-05-03",
+    mobile: "1999999999",
+    addr: "北京朝阳区"
+  }
+])
+const showAddressTable = (type: string) => {
+  showDrawer.value = true
+  if (type === "from") {
+    console.log("from")
+  } else if (type === "to") {
+    console.log("to")
+  }
+}
+</script>
 <template>
-  <div>发布运单</div>
+  <el-card class="box-card" shadow="never" style="min-width: 1120px">
+    <template #header>
+      <div class="card-header">
+        <span style="font-weight: bold; font-size: 18px">发布运单</span>
+      </div>
+    </template>
+    <div style="padding: 0 20px">
+      <div>
+        <h4 style="margin: 5px">地址信息</h4>
+        <el-row :gutter="30">
+          <el-col :span="12">
+            <h5 style="display: flex; align-items: center">
+              <el-icon color="#0088f5" size="24">
+                <Location />
+              </el-icon>
+              发货地
+            </h5>
+            <el-form label-width="80px" style="max-width: 500px">
+              <el-form-item label="发货地址" :error="fromDateError.addr">
+                <el-input v-model="fromDate.from_addr">
+                  <template #append>
+                    <el-button style="font-weight: 400" @click="showAddressTable('from')"> 地址库 </el-button>
+                  </template>
+                </el-input>
+              </el-form-item>
+              <el-row :gutter="10">
+                <el-col :span="10">
+                  <el-form-item label="发货人" :error="fromDateError.from_name">
+                    <el-input v-model="fromDate.from_name" />
+                  </el-form-item>
+                </el-col>
+                <el-col :span="14">
+                  <el-form-item label-width="10" :error="fromDateError.from_mobile">
+                    <el-input v-model="fromDate.from_mobile" />
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <el-form-item label="发货时间" :error="fromDateError.from_date">
+                <el-date-picker
+                  v-model="fromDate.from_date"
+                  type="date"
+                  placeholder="计划发货时间"
+                  :shortcuts="shortcuts"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-form>
+          </el-col>
+          <el-col :span="12">
+            <h5 style="display: flex; align-items: center">
+              <el-icon color="#ff6772" size="24">
+                <Location />
+              </el-icon>
+              收货地
+            </h5>
+            <el-form label-width="80px" style="max-width: 500px">
+              <el-form-item label="收货地址" :error="toDateError.addr">
+                <el-input v-model="toDate.addr">
+                  <template #append>
+                    <el-button style="font-weight: 400" @click="showAddressTable('to')">地址库 </el-button>
+                  </template>
+                </el-input>
+              </el-form-item>
+              <el-row :gutter="10">
+                <el-col :span="10">
+                  <el-form-item label="收货人" :error="toDateError.to_name">
+                    <el-input v-model="toDate.to_name" />
+                  </el-form-item>
+                </el-col>
+                <el-col :span="14">
+                  <el-form-item label-width="10" :error="toDateError.to_mobile">
+                    <el-input v-model="toDate.to_mobile" />
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <el-form-item label="收货时间" :error="toDateError.to_date">
+                <el-date-picker
+                  v-model="toDate.to_date"
+                  type="date"
+                  placeholder="计划发货时间"
+                  :disabled-date="disabledDate"
+                  :shortcuts="shortcuts"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-form>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <el-divider border-style="dotted" />
+    <div>
+      <h4 style="margin-bottom: 20px">货物信息</h4>
+      <el-form label-width="110px">
+        <el-row :gutter="50">
+          <el-col :span="8">
+            <el-form-item label="货物类型" :error="goodInfoError.goods_type">
+              <el-select
+                v-model="goodInfo.goods_type"
+                placeholder="货物类型"
+                style="min-width: 260px; max-width: 260px"
+              >
+                <el-option
+                  v-for="item in goodInfo.goods_type_options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="重量（吨）" :error="goodInfoError.weight">
+              <el-input v-model="goodInfo.weight" style="min-width: 260px; max-width: 260px" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="货物名称" :error="goodInfoError.title">
+              <el-input v-model="goodInfo.title" style="min-width: 260px; max-width: 260px" />
+            </el-form-item>
+
+            <el-form-item label="单价（元/吨）" :error="goodInfoError.unit_price">
+              <el-input v-model="goodInfo.unit_price" style="min-width: 260px; max-width: 260px" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="货物价值" :error="goodInfoError.cost">
+              <el-input v-model="goodInfo.cost" style="min-width: 260px; max-width: 260px" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-form-item label="备注" :error="goodInfoError.remark">
+            <el-input
+              v-model="goodInfo.remark"
+              type="textarea"
+              :autosize="{ minRows: 4 }"
+              style="min-width: 260px; max-width: 260px"
+            />
+          </el-form-item>
+        </el-row>
+      </el-form>
+      <el-row justify="center" align="middle" style="height: 80px">
+        <el-button type="primary" style="width: 200px; height: 40px" @click="doPublish()">发布运单</el-button>
+      </el-row>
+    </div>
+  </el-card>
+  <el-drawer v-model="showDrawer" title="我的地址库" direction="rtl" size="40%">
+    <el-table :data="tableData">
+      <el-table-column property="name" label="姓名" />
+      <el-table-column property="mobile" label="手机号" />
+      <el-table-column property="addr" label="地址" :show-overflow-tooltip="true" />
+      <el-table-column label="选项" fixed="right">
+        <template #default="scope">
+          <el-button size="small" type="primary" @click="doSelectAddress(scope.row)">选择</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-drawer>
 </template>
 
 <style scoped></style>
